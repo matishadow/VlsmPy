c8d0529dfb948815457b56f198d30f0f7dec55e1
matishadow <matishadow@gmail.com>
2017-01-07 22:09:28 +0100

    help function clears screen

--- vlsm.py
+++ vlsm.py
@@ -1,9 +1,15 @@
+import os
 import sys
 
 
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
 def print_help():
+    cls()
     print("VLSM subnetting tool for IPv4")
-    print()
+    print('')
     print("usage: vlsm.py -n network_address -m mask -h hosts")
     print("\n")
     print("Examples:")
@@ -11,3 +17,11 @@ def print_help():
     print("vlsm.py -n 192.168.1.0 -m /24 -h 50,25,100,5")
 
     sys.exit(0)
+
+
+def main():
+    if not len(sys.argv[1:]):
+        print_help()
+
+
+main()
